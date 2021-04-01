5662f8abddbdbce9684b27e6fa89eb750178054a
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-04-01 13:54:12 +0900

    Add copy button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var button = document.querySelector('.btn');
 var text = document.querySelector('.color');
+var icon = document.querySelector('.fa-clipboard');
 function createRandomColor() {
     var choice = "0123456789ABCDEF";
     var answer = "#";
@@ -12,4 +13,16 @@ button.addEventListener('click', function () {
     var color = createRandomColor();
     text.innerText = color;
     document.body.style.backgroundColor = color;
+});
+icon.addEventListener('click', function () {
+    console.log('here');
+    navigator.clipboard.writeText(text.innerText);
+    button.innerText = "Copied!";
+    button.style.backgroundColor = "yellow";
+    button.style.color = "black";
+    setTimeout(function () {
+        button.innerText = "Click Me";
+        button.style.backgroundColor = "black";
+        button.style.color = "white";
+    }, 500);
 });
--- assets/js/app.ts
+++ assets/js/app.ts
@@ -1,10 +1,13 @@
+
 let button: HTMLButtonElement = document.querySelector('.btn')!;
 let text: HTMLSpanElement = document.querySelector('.color')!;
-function createRandomColor(): string{
+let icon: HTMLBaseElement = document.querySelector('.fa-clipboard')!;
+
+function createRandomColor(): string {
     let choice: string = "0123456789ABCDEF";
     let answer: string = "#";
-    for (let i: number = 0; i < 6; i++){
-        answer += choice[Math.floor(Math.random()*16)]
+    for (let i: number = 0; i < 6; i++) {
+        answer += choice[Math.floor(Math.random() * 16)]
     }
     return answer;
 }
@@ -13,4 +16,17 @@ button.addEventListener('click', function () {
     let color: string = createRandomColor();
     text.innerText = color;
     document.body.style.backgroundColor = color;
+})
+
+icon.addEventListener('click', function () {
+    console.log('here');
+    navigator.clipboard.writeText(text.innerText);
+    button.innerText = "Copied!";
+    button.style.backgroundColor = "yellow";
+    button.style.color = "black";
+    setTimeout(function () {
+        button.innerText = "Click Me";
+        button.style.backgroundColor = "black";
+        button.style.color = "white";
+    }, 500);
 })
